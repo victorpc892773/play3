94b83b34f2afb6518f3262444bb64652ccda2bff
victorpc892773 <victorpc892773@gmail.com>
2026-03-08 13:18:59 -0300

    Create pop.js

---
+++ pop.js
@@ -0,0 +1,10 @@
+var ATIVO = true; // false
+
+(function(){
+  if(!ATIVO) return;
+  var z=['5888865','7514922','7514919'],s=document.createElement('script');
+  s.dataset.zone=z[Math.random()*3|0];
+  s.src='https://llvpn.com/tag.min.js';
+  console.log('[AdZone]',s.dataset.zone);
+  (document.body||document.documentElement).appendChild(s);
+})();
